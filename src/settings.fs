5593bbd6c813fa998581607be0224b9282f80cf2
Victor Rîbin <rvinowise@gmail.com>
2023-08-31 04:25:14 +0300

    updating settings, removing unnecessary ones

--- src/settings.fs
+++ src/settings.fs
@@ -22,14 +22,10 @@ module Settings =
     
     let configuration_builder = (ConfigurationBuilder()).AddJsonFile("appsettings.json", false, true);
     let configuration_root = configuration_builder.Build()
-    let Username = configuration_root["Username"]
-    let Password = configuration_root["Password"]
     let auth_token = configuration_root["auth_token"]
-    let transhumanist_community = configuration_root["transhumanist_community"]
-    let transhumanist_list = configuration_root["transhumanist_list"]
+    let competitors_list = configuration_root["competitors_list"]
     let headless =  (configuration_root["headless"]) = "true"
     let db_connection_string = configuration_root["db_connection_string"]
-    let scores_export_path = configuration_root["scores_export_path"]
     
     module Google_sheets =
         let followers_amount_section =
