473d1a5ec95664016eba0c21a3e86c7a7bc783b9
Victor Rîbin <rvinowise@gmail.com>
2023-12-05 00:46:36 +0300

    transposition of tables when writing to google sheet

--- src/googlesheet_interface/Colors.fs
+++ src/googlesheet_interface/Colors.fs
@@ -24,14 +24,12 @@ module Color =
             Alpha= float32 color.alpha
         )
     
-    let to_googlesheet_cell_background (color:Color) =
-        CellFormat(
-            BackgroundColor = Color(
-                Blue = float32 color.blue,
-                Red = float32 color.red,
-                Green = float32 color.green,
-                Alpha = float32 color.alpha
-            )
+    let to_google_color (color:Color) =
+        Color(
+            Blue = float32 color.blue,
+            Red = float32 color.red,
+            Green = float32 color.green,
+            Alpha = float32 color.alpha
         )
 
 module Colors =
